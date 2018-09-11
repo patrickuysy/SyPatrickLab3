12dd345894f2398aab306a8da169f85dc21d4fb3
patrickuysy <2015081400@ust-ics.mygbiz.com>
2018-09-11 15:48:49 +0800

    Initial commit

---
+++ app/src/main/java/sy/patrick/com/sypatricklab3/SyPatrickLab3.java
@@ -0,0 +1,13 @@
+package sy.patrick.com.sypatricklab3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SyPatrickLab3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sy_patrick_lab3);
+    }
+}
